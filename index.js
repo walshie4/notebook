9d75da55abfe4751059b05e3fc10e2a07ccdf98d
Adam Walsh <adamwalsh14@gmail.com>
2016-12-27 21:43:26 -0500

    Hide output to stdout when entering passphrase

--- index.js
+++ index.js
@@ -16,7 +16,55 @@ const rl = readline.createInterface({
 });
 
 /**
- * Set up
+ * Encryption helper functions
+ */
+
+function hiddenInput(question, cb) {
+  let i       = 0;
+  const onData = function(char) {
+    process.stdin.resume();
+    const charStr = char + '';
+    switch (charStr) {
+      case "\n":
+      case "\r":
+      case "\u0004":
+        process.stdin.removeListener('data', onData);
+        break;
+      default:
+        process.stdout.write("\033[2K\033[200D" + question + `[${(i%2===1) ? '=-' : '-='}]`);
+        i += 1;
+        break;
+    }
+  };
+
+  process.stdin.on('data', onData);
+
+  rl.question(question, function(value) {
+    rl.history = rl.history.slice(1);
+    cb(value);
+  });
+};
+
+function encrypt(keyStr, data) {
+  const encKey = crypto.createHash('sha256')
+              .update(keyStr)
+              .digest();
+  cipher = crypto.createCipheriv('aes256', encKey, encKey.slice(0, 16));
+  cipher.end(data, 'utf8');
+  return cipher.read().toString('base64');
+};
+
+function decrypt(keyStr, data) {
+  const encKey = crypto.createHash('sha256')
+              .update(keyStr)
+              .digest();
+  decipher = crypto.createDecipheriv('aes256', encKey, encKey.slice(0, 16));
+  decipher.end(data, 'base64');
+  return decipher.read().toString('utf8');
+};
+
+/**
+ * Start of execution
  */
 
 if (cli.verbose) console.log('Starting up.');
@@ -30,10 +78,10 @@ if (!fs.existsSync(outputDir)) {
 const keyFile = process.env.WRITE_ENC_KEY || path.join(outputDir, '.writeLandKey');
 let key;
 
-rl.question('Enter encryption passphrase: ', (passwd) => {
+hiddenInput('Enter encryption passphrase: ', function (passwd) {
   // First run, no key file
   if (!fs.existsSync(keyFile)) {
-    crypto.randomBytes(4096, (err, buf) => {
+    crypto.randomBytes(4096, function (err, buf) {
       key = buf.toString('hex');
       const keyData = `Passwd:${key}dwssaP`;
       fs.writeFileSync(keyFile, encrypt(passwd, keyData), 'utf8');
@@ -42,6 +90,7 @@ rl.question('Enter encryption passphrase: ', (passwd) => {
     if (cli.verbose) console.log('Decrypting keyfile');
     const keyData = decrypt(passwd, fs.readFileSync(keyFile, 'utf8'));
     if (/^Passwd\:.*dwssaP$/.test(keyData)) {
+      if (cli.verbose) console.log('Keyfile decrypted successfully');
       key = keyData.slice(7, -6);
     } else {
       console.log('Decrypting keyfile failed');
@@ -52,7 +101,7 @@ rl.question('Enter encryption passphrase: ', (passwd) => {
   parseOptions();
 });
 
-const parseOptions = () => {
+function parseOptions() {
 
   if (cli.verbose) console.log(`Using output dir ${outputDir}`);
 
@@ -64,45 +113,28 @@ const parseOptions = () => {
     // Decrypt and show entry
     if (cli.verbose) console.log('Reading output directory');
     // Read through save dir and list entries
-    let notes = fs.readdirSync(outputDir);
-    notes = notes.filter((note) => note !== '.writeLandKey');
+    const files = fs.readdirSync(outputDir);
+    const notes = files.filter(function (note) { return note !== '.writeLandKey' });
+    if (!notes.length) console.log(`No notes found @ ${outputDir}`);
     notes.forEach(function (note) {
       if (cli.verbose) console.log(`Reading note ${note}`);
       const fileData = fs.readFileSync(path.join(outputDir, note), 'utf8');
+      if (cli.verbose) console.log(`Decrypting note from ${fileData}`);
       console.log(decrypt(key, fileData));
     });
     rl.close();
+    process.exit(1);
 
   } else {
     // Take and save new entry
-    rl.question('Enter new note: ', (resp) => {
+    rl.question('Enter new note: ', function (resp) {
       const outputPath = path.join(outputDir, new Date().toISOString());
       const data = encrypt(key, resp);
       if (cli.verbose) console.log(`Writing note ${data} to note file ${outputPath}`);
       fs.writeFileSync(outputPath, data, 'utf8');
       rl.close();
+      process.exit(1);
     });
   }
 };
 
-/**
- * Encryption helper functions
- */
-
-const encrypt = function(keyStr, data) {
-  const encKey = crypto.createHash('sha256')
-              .update(keyStr)
-              .digest();
-  cipher = crypto.createCipheriv('aes256', encKey, encKey.slice(0, 16));
-  cipher.end(data, 'utf8');
-  return cipher.read().toString('base64');
-};
-
-const decrypt = function(keyStr, data) {
-  const encKey = crypto.createHash('sha256')
-              .update(keyStr)
-              .digest();
-  decipher = crypto.createDecipheriv('aes256', encKey, encKey.slice(0, 16));
-  decipher.end(data, 'base64');
-  return decipher.read().toString('utf8');
-};
